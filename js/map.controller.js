6b7f6926b26f817d5e3ee4a2936b33471cc43566
Aviad Ben-Hamo <aviad.benhamo@gmail.com>
2025-11-05 19:15:41 +0200

    feat: Add places via map click and pan-to-place

--- js/map.controller.js
+++ js/map.controller.js
@@ -14,15 +14,25 @@ function onInit() {
 
 function initMap() {
     const elatCoords = { lat: 29.5581, lng: 34.9482 }
-
     const mapOptions = {
         center: elatCoords,
         zoom: 12
     }
-
     const elMap = document.querySelector('.map-container')
-
     gMap = new google.maps.Map(elMap, mapOptions)
+
+    gMap.addListener('click', (ev) => {
+        const placeName = prompt('Enter place name:', 'New Place')
+        if (!placeName) return
+
+        const lat = ev.latLng.lat()
+        const lng = ev.latLng.lng()
+
+        placeService.addPlace(placeName, lat, lng, gMap.getZoom())
+
+        renderPlaces()
+        gMap.setCenter({ lat, lng })
+    })
 }
 
 function renderPlaces() {
@@ -49,6 +59,7 @@ function onRemovePlace(placeId) {
 }
 
 function onPanToPlace(placeId) {
-    console.log('User wants to pan to:', placeId)
-
+    const place = placeService.getPlaceById(placeId)
+    gMap.setCenter({ lat: place.lat, lng: place.lng })
+    gMap.setZoom(place.zoom)
 }
--- js/placeService.js
+++ js/placeService.js
@@ -1,10 +1,14 @@
 'use strict'
 
+const STORAGE_KEY = 'placeDB'
+
 var gPlaces
 
 const placeService = {
     getPlaces,
     removePlace,
+    addPlace,
+    getPlaceById
 }
 
 _createPlaces()
@@ -15,20 +19,36 @@ function getPlaces() {
 
 function removePlace(placeId) {
     const placeIdx = gPlaces.findIndex(place => place.id === placeId)
-
     gPlaces.splice(placeIdx, 1)
+
+    _savePlacesToStorage()
+}
+
+function addPlace(name, lat, lng, zoom) {
+    const newPlace = _createPlace(name, lat, lng, zoom)
+    gPlaces.unshift(newPlace)
+
+    _savePlacesToStorage()
+}
+
+function getPlaceById(placeId) {
+    return gPlaces.find(place => place.id === placeId)
 }
 
 
 function _createPlaces() {
-    gPlaces = [
-        _createPlace('Sydney', -33.8688, 151.2093, 10),
-        _createPlace('Florentin', 32.0558, 34.7709, 15)
-    ]
+    gPlaces = loadFromStorage(STORAGE_KEY)
+
+    if (!gPlaces || !gPlaces.length) {
+        gPlaces = [
+            _createPlace('Sydney', -33.8688, 151.2093, 10),
+            _createPlace('Florentin', 32.0558, 34.7709, 15)
+        ]
+        _savePlacesToStorage()
+    }
 }
 
 function _createPlace(name, lat, lng, zoom) {
-
     return {
         id: _makeId(),
         name,
@@ -36,6 +56,10 @@ function _createPlace(name, lat, lng, zoom) {
         lng,
         zoom
     }
+}
+
+function _savePlacesToStorage() {
+    saveToStorage(STORAGE_KEY, gPlaces)
 }
 
 function _makeId(length = 4) {
